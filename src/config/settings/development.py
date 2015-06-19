fd2c1a59cfc120ac0cc193f6e863fc9501e62fb2
Alex <alexandr@gavrisco.com>
2015-06-19 15:21:13 +0300

    Add development settings.

---
+++ src/config/settings/development.py
@@ -0,0 +1,10 @@
+from config.settings.base import *  # flake:8 noq
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+INSTALLED_APPS += (
+    # Here you can place apps used only in development,
+    # for example:
+    # 'debug_toolbar.apps.DebugToolbarConfig',
+)
