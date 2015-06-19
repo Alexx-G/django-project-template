720e398ac6d21c2e394dda08d47d5110b3a38c40
Alex <alexandr@gavrisco.com>
2015-06-19 14:51:11 +0300

    Add sample file with secrets (db credentials and secret_key).

---
+++ src/config/settings/secrets_sample.py
@@ -0,0 +1,16 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ''
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
+        'USER': '',
+        'PASSWORD': ''
+    }
+}
